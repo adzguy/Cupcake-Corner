7bcb54ef740e9504777f94e0f3e26ac02bce6f79
Davron <devrannur@gmail.comn>
2020-01-04 17:58:52 -0600

    Use of disabled() in Forms.

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -8,61 +8,30 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
     
-    @State private var results = [Result]()
+    var disabledForm: Bool {
+        username.count < 5 || email.count < 5 
+        
+    }
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        }
-        .onAppear(perform: loadData)
-    }
-    
-    func loadData() {
-        // Step 1. Creaing URL we want to read
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        // Step 2. Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed
-        let request = URLRequest(url: url)
-        
-        // Step 3. Create and start a network task from that URL request.
-        URLSession.shared.dataTask(with: request) {data, request, error in
-            // Step 4. Handle the result of that networking task
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data - go back to the mian thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
-                    
-                    // everything is good, so we can exit
-                    return
+            Section{
+                Button("Create Account") {
+                    print("Creating account...")
                 }
             }
-            
-            // if we are still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+        .disabled(disabledForm)
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
